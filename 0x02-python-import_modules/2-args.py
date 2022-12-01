cb62f3a04bf395947a9a3f4e4fa3c5d6635ab424
dennisnderitu254 <dknderitu@gmail.com>
2022-12-01 13:55:38 +0300

    removing white space

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     """ Print the number of and list of arguments."""
     import sys
-    
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
