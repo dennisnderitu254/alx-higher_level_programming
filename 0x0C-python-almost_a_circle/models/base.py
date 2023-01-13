5981fe1de19775868479f79771506a3bb72c0a57
dennisnderitu254 <dknderitu@gmail.com>
2023-01-13 13:32:15 +0300

    debugging base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,5 +17,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb__objects += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
