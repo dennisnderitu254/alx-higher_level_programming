324c7dafc0f2a9200fbad2166063497986191283
dennisnderitu254 <dknderitu@gmail.com>
2023-01-17 11:51:02 +0300

    Debugging Code Base - Tests and Models - and adding main files for testing

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-""" Module that contains class Rectangle,
-inheritance of class Base
-"""
+""" module that contains form class rectangle inherits from class base"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ Class Rectangle """
+    """ class rectangle """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
+        """ creating the init function
 
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
-        Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
+            @width: the width of the rectangle
+            @height: the height of the rectangle
+            @x: x
+            @y: y
+            @id: id
         """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
@@ -107,17 +100,18 @@ class Rectangle(Base):
 
         return rectangle + rec_id + rect_xy + rec_wh
 
-    def display(self):
-        """Print the Rectangle using the `#` character."""
-        if self.width == 0 or self.height == 0:
-            print("")
-            return
+    def update(self, *args, **kwargs):
+        """ updating class rectangle that assigns an argument"""
+        count = 0
+        if args is not None and len(args) != 0:
+            list_arguments = ["id", "width", "height", "x", "y"]
 
-        [print("") for y in range(self.y)]
-        for h in range(self.height):
-            [print(" ", end="") for x in range(self.x)]
-            [print("#", end="") for w in range(self.width)]
-            print("")
+            for values in args:
+                setattr(self, list_arguments[count], values)
+                count += 1
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
         """method that returns a dictionary representation of a rectangle"""
