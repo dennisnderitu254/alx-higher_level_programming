47c620d51cdbde7d2c9863ee627948451b3cd437
dennisnderitu254 <dknderitu@gmail.com>
2023-03-13 12:48:09 +0300

    debugging code

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-// Declare a constant variable with the value "JavaScript is amazing"
-const myVar = 'JavaScript is amazing'
-// Output the value of the constant variable
-console.log(myVar) // Output: JavaScript is amazing
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-// Print the first line
-console.log('C is fun')
-// Print the second line
-console.log('Python is cool')
-// Print the third line
-console.log('JavaScript is amazing')
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
