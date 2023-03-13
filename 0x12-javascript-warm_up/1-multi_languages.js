ce9678ec1cb9a0e9381b170f679af523b14f69c0
dennisnderitu254 <dknderitu@gmail.com>
2023-03-13 12:41:36 +0300

    linting code using npx standard

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Print the first line
-console.log('C is fun');
+console.log('C is fun')
 // Print the second line
-console.log('Python is cool');
+console.log('Python is cool')
 // Print the third line
-console.log('JavaScript is amazing');
+console.log('JavaScript is amazing')
