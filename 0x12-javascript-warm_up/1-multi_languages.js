f34ebe34bb4389b483d363281b5d10e3873845fe
dennisnderitu254 <dknderitu@gmail.com>
2023-03-13 12:15:03 +0300

    Writing a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Print the first line
+console.log('C is fun');
+// Print the second line
+console.log('Python is cool');
+// Print the third line
+console.log('JavaScript is amazing');
