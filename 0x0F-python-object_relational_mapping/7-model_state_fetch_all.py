f267621b46f0653b0b0308157c8fe603a0e13ce2
dennisnderitu254 <dknderitu@gmail.com>
2023-03-18 23:37:30 +0300

    solving mandatory and advanced tasks

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,12 +9,7 @@ Usage: ./7-model_state_fetch_all.py <mysql username> /
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
