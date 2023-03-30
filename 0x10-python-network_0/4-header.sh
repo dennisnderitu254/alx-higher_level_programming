82ff2b05542843095c23720d57fd208235ccb5aa
dennisnderitu254 <dknderitu@gmail.com>
2023-03-30 15:40:33 +0300

    Send a GET request to a given URL with a header variable

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# curl sends GET req to URL, displays response body
+curl -sH "X-School-User-Id: 98" "$1"
