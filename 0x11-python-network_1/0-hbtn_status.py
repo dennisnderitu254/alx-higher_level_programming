c94089563fbbc12032c68b636dbbf5b18572b849
dennisnderitu254 <dknderitu@gmail.com>
2023-03-31 22:31:39 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-from urllib import request
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
+
 
 if __name__ == "__main__":
+    from urllib import request
+
     with request.urlopen("https://intranet.hbtn.io/status") as response:
         response = response.read()
         print("Body response:")
